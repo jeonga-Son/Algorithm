638a4db36d7328d9eb5cf15c11c3ee13d8006e76
jeonga-Son <107593020+jeonga-Son@users.noreply.github.com>
2022-12-12 01:13:08 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] dot) {
+        int answer = 0;
+        
+        for (int i = 0; i < 2; i++) {
+            if (dot[i] >= -500 && dot[i] <= 500) {
+                if (dot[0] > 0 && dot[1] > 0) {
+                    answer = 1;
+                }
+                else if(dot[0] < 0 && dot[1] > 0) {
+                    answer = 2;
+                }
+
+                else if (dot[0] < 0 && dot[1] < 0) {
+                    answer = 3;
+                }
+                else {
+                    answer = 4;
+                }
+            }
+        }
+        return answer;
+    }
+}
