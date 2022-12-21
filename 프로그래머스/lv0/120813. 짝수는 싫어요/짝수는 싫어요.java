a4239e40733e64c01ac92cc7702a3cbf65634979
jeonga-Son <107593020+jeonga-Son@users.noreply.github.com>
2022-12-21 11:10:32 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.84 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,19 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int n) {
-        int[] answer;
-        
-        if (n % 2 == 0) {
-            answer = new int[n / 2];
-        } else {
-            answer = new int[n / 2 + 1];
-        }
-        
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 1) {
-                answer[i / 2] = i;
-            }
-        }
-        
-        return answer;
+       return IntStream.rangeClosed(0, n).filter(value -> value % 2 == 1).toArray();
     }
 }
