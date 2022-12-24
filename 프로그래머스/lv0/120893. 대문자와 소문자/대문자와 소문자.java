7f69787c2cfdd05e7f36bbb1b63ab97064e6564c
jeonga-Son <107593020+jeonga-Son@users.noreply.github.com>
2022-12-24 12:42:51 +0900

    [level 0] Title: 대문자와 소문자, Time: 7.78 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        char[] array = my_string.toCharArray();
+        
+        String temp = "";
+        
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] >= 97 && array[i] <= 122) {
+                temp = array[i] + "";
+                answer += temp.toUpperCase();
+            } else {
+                temp = array[i] + "";
+                answer += temp.toLowerCase();
+            }
+        }
+        return answer;
+    }
+}
