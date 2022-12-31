23ae48821b42a1b1239f3c897b40c24a3c3cd6c5
jeonga-Son <107593020+jeonga-Son@users.noreply.github.com>
2023-01-01 01:03:12 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -2,37 +2,41 @@ class Solution {
     public int[] solution(String[] keyinput, int[] board) {
         int[] answer = new int[2];
         int count = 0;
+        int leftLimit = -(board[0] / 2);
+        int rightLimit = board[0] / 2;
+        int upLimit = board[1] / 2;
+        int downLimit = -(board[1] / 2);
         
         for (int i = 0; i < keyinput.length; i++) {
             if (keyinput[i].equals("left")) {
                 answer[0] -= 1;
                 
-                if (answer[0] < -(board[0] / 2)) {
-                    answer[0] = -(board[0] / 2);
+                if (answer[0] < leftLimit) {
+                    answer[0] = leftLimit;
                 }
             } 
             
             if (keyinput[i].equals("right")) {
                 answer[0] += 1;
                 
-                if (answer[0] > board[0] / 2) {
-                    answer[0] = board[0] / 2;
+                if (answer[0] > rightLimit) {
+                    answer[0] = rightLimit;
                 }
             } 
             
             if (keyinput[i].equals("up")) {
                 answer[1] += 1;
                 
-                if (answer[1] > board[1] / 2) {
-                    answer[1] = board[1] / 2;
+                if (answer[1] > upLimit) {
+                    answer[1] = upLimit;
                 }
             } 
             
             if (keyinput[i].equals("down")) {
                 answer[1] -= 1;
                 
-                if (answer[1] < -(board[1]) / 2) {
-                    answer[1] = -(board[1] / 2);
+                if (answer[1] < downLimit) {
+                    answer[1] = downLimit;
                 }
             }
         }
