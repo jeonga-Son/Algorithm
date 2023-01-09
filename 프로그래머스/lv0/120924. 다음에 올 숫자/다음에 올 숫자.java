7c086f2e7f37606f357060e92df9fc484e3bc6c3
jeonga-Son <107593020+jeonga-Son@users.noreply.github.com>
2023-01-09 14:13:24 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,14 +1,11 @@
 class Solution {
     public int solution(int[] common) {
         int answer = 0;
-        int[] num = new int[common.length - 1];
+        int x = common[1] - common[0];
+        int y = common[2] - common[1];
         
-        for (int i = 1; i < common.length; i++) {
-            num[i - 1] = common[i] - common[i - 1];
-        }
-        
-        if (num[0] == num[1]) {
-            answer = common[common.length - 1] + num[0];
+        if (x == y) {
+            answer = common[common.length - 1] + x;
         } else {
             answer = common[common.length - 1] * (common[1] / common[0]);
         }
