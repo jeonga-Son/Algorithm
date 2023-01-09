8222e572947c94200b5831fdaf73e7a29908415d
jeonga-Son <107593020+jeonga-Son@users.noreply.github.com>
2023-01-09 15:10:31 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.10 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,46 +1,20 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = new int[2];
-        int value = 0;
+        answer[0] = (numer1 * denom2) + (numer2 * denom1);
+        answer[1] = denom1 * denom2;
         
-        if (denom1 % denom2 == 0) {
-            value = denom1 / denom2;
-            answer[0] = numer1 + (numer2 * value);
-            answer[1] = denom1;
+        int min = Math.min(answer[0], answer[1]);
+        int max = 1;
+        
+        for (int i = 1; i <= min; i++) {
+           if (answer[0] % i == 0 && answer[1] % i == 0) {
+               max = i;
+           }
         }
         
-        if (denom2 % denom1 == 0) {
-            value = denom2 / denom1;
-            answer[0] = numer2 + (numer1 * value);
-            answer[1] = denom2;
-        }
-        
-        if (denom1 == denom2) {
-            answer[0] = numer1 + numer2;
-            answer[1] = denom1;
-            
-            if (answer[0] == answer[1]) {
-                answer[0] = 1;
-                answer[1] = 1;
-            }
-        } 
-        
-        if (denom1 % denom2 != 0 && denom2 % denom1 != 0) {
-            answer[0] = (numer1 * denom2) + (numer2 * denom1);
-            answer[1] = denom1 * denom2;
-        }
-        
-        for(int i = 2; i <= answer[0]; i++) {
-            if (answer[0] % i == 0 && answer[1] % i == 0) {
-                answer[0] = answer[0] / i;
-                answer[1] = answer[1] / i;
-            }
-            
-            if (answer[0] == i && answer[1] == i) {
-                answer[0] = 1;
-                answer[1] = 1;
-            }
-        }
+        answer[0] /= max;
+        answer[1] /= max;
         
         return answer;
     }
