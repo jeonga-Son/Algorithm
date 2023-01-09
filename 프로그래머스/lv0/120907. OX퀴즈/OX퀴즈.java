d71700dd017eeb085bec43b6d6d581df345c5d62
jeonga-Son <107593020+jeonga-Son@users.noreply.github.com>
2023-01-09 12:15:06 +0900

    [level 0] Title: OX퀴즈, Time: 0.09 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        String[] arr = new String[quiz.length];
+        
+        for (int i = 0; i < quiz.length; i++) {
+            arr = quiz[i].split(" ");
+            if (arr[1].equals("-")) {
+                if (Integer.parseInt(arr[0]) - Integer.parseInt(arr[2]) == Integer.parseInt(arr[4])) {
+                    answer[i] = "O";
+                } else {
+                    answer[i] = "X";
+                }
+            }
+            
+            if (arr[1].equals("+")) {
+                if(Integer.parseInt(arr[0]) + Integer.parseInt(arr[2]) == Integer.parseInt(arr[4])) {
+                    answer[i] = "O";
+                } else {
+                    answer[i] = "X";
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
