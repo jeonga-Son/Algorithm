37c610b43aaa0b9b1f0282f3b43f172ccb9fb5fe
jeonga-Son <107593020+jeonga-Son@users.noreply.github.com>
2023-01-14 20:27:03 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,15 +1,9 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
+        boolean answer = false;
         
         if (s.length() == 4 || s.length() == 6) {
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) < '0' || s.charAt(i) > '9') {
-                    return false;
-                }
-            }
-        } else {
-            return false;
+            answer = s.matches("^[0-9]*$");
         }
         
         return answer;
