04abb3ef2f5c24607deeb9efc36f8faa391d3be2
jeonga-Son <107593020+jeonga-Son@users.noreply.github.com>
2023-01-31 18:58:45 +0900

    [level 1] Title: K번째수, Time: 0.32 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -3,10 +3,14 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
+        
         for (int i = 0; i < commands.length; i++) {
-            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            int[] temp = new int[commands[i][1] - (commands[i][0] - 1)];
+            for(int j = 0; j < temp.length; j++) {
+                temp[j] = array[j + (commands[i][0] - 1)];
+            }
             
-            Arrays.sort(temp); 
+            Arrays.sort(temp);
             
             answer[i] = temp[commands[i][2] - 1];
         }
